336d95140f189ae5e8422a6afbb7780256119bd4
nawers <naw.brice@gmail.com>
2025-05-30 12:07:35 +0200

    Update myApp.js
    fix

--- myApp.js
+++ myApp.js
@@ -3,14 +3,11 @@
 
 const express = require('express');
 const bodyParser = require("body-parser");
-const fs = require('fs');
-const path = require('path');
 const multer = require('multer');
 const app = express();
 
 // Use memory storage or disk storage
 const upload = multer({ dest: 'uploads/' });
-
 
 app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
     
